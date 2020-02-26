4af6a5f5f69d16232578edf331e7914ba10e8918
HifzaZaheer96 <Hifza.Zaheer@academytrainee.com>
2020-02-26 14:21:28 +0000

    Restrict User Input

--- RPS.py
+++ RPS.py
@@ -23,34 +23,40 @@ while game:
 
     player_decision = int(input("Please enter, 0 for Rock, 1 for Paper or 2 for Scissors: "))
 
-    computer_decision = computer_learning()
+    if (player_decision == 0 or player_decision ==  1 or player_decision == 2):
+   
+        computer_decision = computer_learning()
 
-    if player_decision == computer_decision:
-        print("Tie!")
+        if player_decision == computer_decision:
+            print("Tie!")
 
-    elif player_decision == 0 and computer_decision == 2:
-        rock_counter +=1
-        print("You win")
-    
-    elif player_decision == 1 and computer_decision == 0:
-        paper_counter +=1
-        print("You win")
+        elif player_decision == 0 and computer_decision == 2:
+            rock_counter +=1
+            print("You win")
+        
+        elif player_decision == 1 and computer_decision == 0:
+            paper_counter +=1
+            print("You win")
 
-    elif player_decision == 2 and computer_decision == 1:
-        scissors_counter +=1
-        print("You win")
+        elif player_decision == 2 and computer_decision == 1:
+            scissors_counter +=1
+            print("You win")
 
+        else:
+            print("You lose")
+        
+        print("You chose: " + rps[player_decision] + " and the computer chose " + rps[computer_decision])
+
+        do_you_want_to_continue = input("Do you want to continue? (y/n) ")
+        
+        if do_you_want_to_continue == "y":
+            pass
+        elif  do_you_want_to_continue == "n":
+            print("Bye bye")
+            game = False
+        else:
+            print("You did not put in a correct answer? So we will continue")
     else:
-        print("You lose")
-    
-    print("You chose: " + rps[player_decision] + " and the computer chose " + rps[computer_decision])
+        print ("Please pick a number between 0 and 2")
 
-    do_you_want_to_continue = input("Do you want to continue? (y/n) ")
-    
-    if do_you_want_to_continue == "y":
-        pass
-    elif  do_you_want_to_continue == "n":
-        print("Bye bye")
-        game = False
-    else:
-        print("You did not put in a correct answer? So we will continue")
+
